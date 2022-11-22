e6e70a36af8fa6f069db24720d5f260da0befa91
takoyaki-3 <mail@takoyaki3.com>
2022-11-22 19:27:50 +0900

    add filelist

---
+++ exec/getfiles.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"log"
+
+	filetool "github.com/takoyaki-3/file-tool"
+	csvtag "github.com/takoyaki-3/go-csv-tag/v3"
+)
+
+func main() {
+
+	err, fileList := filetool.DirWalk("/",filetool.DirWalkOption{})
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	err = csvtag.DumpToFile(fileList,"files.csv")
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
